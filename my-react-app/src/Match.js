dc08672ffdbf243f5c729317fdaace3ec8d4278f
elisemcclain <celisemcclain@gmail.com>
2023-07-14 15:32:33 -0500

    tinder

--- my-react-app/src/Match.js
+++ my-react-app/src/Match.js
@@ -9,10 +9,11 @@ function Match({ candidate }) {
         <img src={candidate.image} alt={"Your next soulmate"} className ="card_image"/>
         <div className= "card_content">
         <div className="card_title">{candidate.name}</div>
+        <div className="card_text">
         <h4>{candidate.city}</h4>
         <p>{candidate.bio}</p>
         <p>Interests: {candidate.interests}</p>
-        <em>*Dealbreaker: {candidate.dealbreakers}*</em>
+        <em>*Dealbreaker: {candidate.dealbreakers}*</em></div>
         <div className="card_detail">
         <button className="chat">Let's Chat!</button>
         <button className="hearted">♡</button></div></div>
